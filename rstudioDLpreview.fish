b6c0e8d46e1dbb7f329a5353c27640f730895680
Orlando <101560994+olayabucaro@users.noreply.github.com>
2022-04-12 16:10:02 +0200

    Create rstudioDLpreview.fish

---
+++ rstudioDLpreview.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/env fish
+
+echo "Downloading and installing latest Rstudio Preview"
+
+# Find dl URL of latest Rstuido preview version
+set rstudiourl (curl https://www.rstudio.com/products/rstudio/download/preview | grep -o -E 'href="([^"#]+)"' | grep -o -E '(http|https)://s3.amazonaws.com/rstudio-ide-build/desktop/bionic/amd64/rstudio-[^/"]+-amd64.deb' | head -1)
+ 
+# Download the file
+curl -O "$rstudiourl"
+ 
+# Get file name 
+set rstudiofile (string split -r -m1 / $rstudiourl |tail -1)
+
+# Install latest Rstudio
+sudo gdebi $rstudiofile
+
+# Remove instalation file
+rm $rstudiofile
